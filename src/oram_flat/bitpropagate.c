fc5e9d6a8f7e20fa97cf295eab701410b1ead0bf
Jack Doerner <j@ckdoerner.net>
2016-11-16 13:32:40 -0500

    Reorganized reveals for improved efficiency

--- src/oram_flat/bitpropagate.c
+++ src/oram_flat/bitpropagate.c
@@ -21,9 +21,12 @@ void bitpropagator_offline_start(bitpropagator_offline * bpo, void * blocks) {
 }
 
 void bitpropagator_offline_push_Z(bitpropagator_offline * bpo, void * Z, uint32_t advicebit, size_t level) {
-	memcpy(&bpo->Z[(level- bpo->startlevel - 1)*BLOCKSIZE], Z, BLOCKSIZE);
-	bpo->advicebits[level- bpo->startlevel - 1] = advicebit;
-	omp_unset_lock(&bpo->locks[level- bpo->startlevel - 1]);
+	#pragma omp task
+	{
+		memcpy(&bpo->Z[(level- bpo->startlevel - 1)*BLOCKSIZE], Z, BLOCKSIZE);
+		bpo->advicebits[level- bpo->startlevel - 1] = advicebit;
+		omp_unset_lock(&bpo->locks[level- bpo->startlevel - 1]);
+	}
 }
 
 void bitpropagator_offline_readblockvector(void * local_output, bitpropagator_offline * bpo) {
