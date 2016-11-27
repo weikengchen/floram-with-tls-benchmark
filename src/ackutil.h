8ce12beecc57a35dfaf9956ec4d547d035ff119f
Jack Doerner <j@ckdoerner.net>
2016-11-27 00:57:42 -0500

    Bits needed for SMPL

--- src/ackutil.h
+++ src/ackutil.h
@@ -4,12 +4,16 @@
 #include <stdint.h>
 #include <obliv_common.h>
 
-#define max(a,b) \
+//Note: the following macros CANNOT be used with obliv arguments (in case it wasn't obivious).
+
+#define LOG2LL(X) ((unsigned) (8*sizeof (unsigned long long) - __builtin_clzll((X)) - 1))
+
+#define MAX(a,b) \
    ({ __typeof__ (a) _a = (a); \
        __typeof__ (b) _b = (b); \
      _a > _b ? _a : _b; })
 
-#define min(a,b) \
+#define MIN(a,b) \
    ({ __typeof__ (a) _a = (a); \
        __typeof__ (b) _b = (b); \
      _a < _b ? _a : _b; })
