0ff0cc00466fa52fca9016f9abd6e89ae2d91051
Jack Doerner <j@ckdoerner.net>
2017-01-16 14:10:34 -0500

    Bugfixes and alignments

--- src/oram_flat/scanrom.c
+++ src/oram_flat/scanrom.c
@@ -1,45 +1,56 @@
 #include "scanrom.h"
 #include "flatoram_util.h"
 
-void scanrom_create_local_halfpad(void * dest, void * key, size_t size) {
+void scanrom_create_local_halfpad(uint8_t * dest, uint8_t * key, size_t size) {
 	offline_expand(dest, key, size);
 }
 
-void scanrom_read_with_bitvector_offline(void* data, void* local_data, bool * bitvector, size_t fullblocksize, size_t blockcount) {
+void scanrom_read_with_bitvector_offline(uint8_t * data, uint8_t * local_data, bool * bitvector, size_t fullblocksize, size_t blockcount) {
 	memset(data, 0, fullblocksize);
+
+	uint64_t * d = local_data;
+	bool * b = bitvector;
 
 	for (size_t jj = 0; jj < fullblocksize /sizeof(uint64_t); jj++) {
 		uint64_t sum = 0;
-		#pragma omp parallel for reduction (^:sum)
+		#pragma omp parallel for simd aligned(d,b:16) reduction(^:sum)
 		for (size_t ii = 0; ii < blockcount; ii++) {
-			if (bitvector[ii]) {
-				sum ^= ((uint64_t *)local_data)[ii * ((fullblocksize) /sizeof(uint64_t)) + jj];
+			if (b[ii]) {
+				sum ^= d[ii * ((fullblocksize) /sizeof(uint64_t)) + jj];
 			}
 		}
 		((uint64_t *)data)[jj] = sum;
 	}
 }
 
-void scanrom_encrypt_offline(void * out, void * in, void* pad, size_t len) {
-	#pragma omp parallel for
+void scanrom_encrypt_offline(uint8_t * out, uint8_t * in, uint8_t* pad, size_t len) {
+	uint64_t * o = out;
+	uint64_t * i = in;
+	uint64_t * p = pad;
+
+	#pragma omp parallel for simd aligned(o,i,p:16)
 	for (size_t ii = 0; ii < len / sizeof(uint64_t); ii++) {
-		((uint64_t *)out)[ii] = ((uint64_t *)in)[ii] ^ ((uint64_t *)pad)[ii];
+		o[ii] = i[ii] ^ p[ii];
 	}	
 }
 
 
 void scanwrom_write_with_blockvector_offline(uint8_t * local_data, uint8_t * blockvector, bool * controlbitvector, uint8_t*Zblock, bool expand, size_t fullblocksize, size_t blockcount) {
+	uint8_t * d = local_data;
+	uint8_t * b = blockvector;
+	uint8_t * z = Zblock;
+
 	#pragma omp parallel for
 	for (size_t ii = 0; ii< blockcount; ii++) {
 		if (controlbitvector[ii]) {
-			#pragma omp simd
+			#pragma omp simd aligned(d,b,z:16)
 			for (size_t jj = 0; jj < fullblocksize; jj++) {
-				local_data[ii * fullblocksize + jj] ^= blockvector[ii * fullblocksize + jj] ^ Zblock[jj];
+				d[ii * fullblocksize + jj] ^= b[ii * fullblocksize + jj] ^ z[jj];
 			}
 		} else {
-			#pragma omp simd
+			#pragma omp simd aligned(d,b:16)
 			for (size_t jj = 0; jj < fullblocksize; jj++) {
-				local_data[ii * fullblocksize + jj] ^= blockvector[ii * fullblocksize + jj];
+				d[ii * fullblocksize + jj] ^= b[ii * fullblocksize + jj];
 			}
 		}
 	}
--- src/oram_flat/scanrom.h
+++ src/oram_flat/scanrom.h
@@ -6,11 +6,11 @@ typedef void (* duplexer_fn)(void *, void *);
 
 void scanrom_transfer_duplexer(duplexer_fn fn1, duplexer_fn fn2, void* data, void * pd);
 
-void scanrom_create_local_halfpad(void * dest, void * key, size_t size);
+void scanrom_create_local_halfpad(uint8_t * dest, uint8_t * key, size_t size);
 
-void scanrom_read_with_bitvector_offline(void* data, void* local_data, bool * bitvector, size_t fullblocksize, size_t blockcount);
+void scanrom_read_with_bitvector_offline(uint8_t * data, uint8_t * local_data, bool * bitvector, size_t fullblocksize, size_t blockcount);
 
-void scanrom_encrypt_offline(void * out, void * in, void* key, size_t len);
+void scanrom_encrypt_offline(uint8_t * out, uint8_t * in, uint8_t* pad, size_t len);
 
 void scanwrom_write_with_blockvector_offline(uint8_t * local_data, uint8_t * blockvector, bool * bitvector, uint8_t*Zblock, bool expand, size_t fullblocksize, size_t blockcount);
 
